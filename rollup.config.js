f4ae086c08260d7eeeb15e26cc52fc1e5174099a
Lukas Jacomin <lukasjacomin@gmail.com>
2022-09-24 02:15:24 -0300

    rollup fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 
 export default {
   input: "src/index.ts",
@@ -8,5 +10,5 @@ export default {
     format: "cjs",
     sourcemap: true,
   },
-  plugins: [typescript({ module: "ESNext" }), resolve()],
+  plugins: [json(), commonjs(), typescript({ module: "ESNext" }), resolve()],
 };
