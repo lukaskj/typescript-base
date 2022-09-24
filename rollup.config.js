196ed16eece0d3cdc750966ee7236b8c90d9c127
Lukas Jacomin <lukasjacomin@gmail.com>
2022-09-24 01:52:02 -0300

    rollup support

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+
+export default {
+  input: "src/index.ts",
+  output: {
+    dir: "dist",
+    format: "cjs",
+    sourcemap: true,
+  },
+  plugins: [typescript({ module: "ESNext" }), resolve()],
+};
