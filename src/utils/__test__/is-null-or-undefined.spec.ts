1e8ba18114d0f37f74e6ff90d5f58b6537e2dce0
Lukas Jacomin <lukasjacomin@gmail.com>
2023-04-12 16:59:24 -0300

    helper coverage

--- src/utils/__test__/is-null-or-undefined.spec.ts
+++ src/utils/__test__/is-null-or-undefined.spec.ts
@@ -1,5 +1,5 @@
 import { faker } from "@faker-js/faker";
-import { isNullOrUndefined } from "../is-null-or-undefined";
+import { isNullOrEmptyOrUndefined, isNullOrUndefined } from "../is-null-or-undefined";
 
 describe("Application", () => {
   describe("Helpers", () => {
@@ -24,6 +24,48 @@ describe("Application", () => {
         const data = faker.random.word();
         const expected = false;
         const result = isNullOrUndefined(data);
+
+        expect(result).toStrictEqual(expected);
+      });
+    });
+
+    describe("isNullOrEmptyOrUndefined", () => {
+      it("Should return true if input is null", () => {
+        const data = null;
+        const expected = true;
+        const result = isNullOrEmptyOrUndefined(data);
+
+        expect(result).toStrictEqual(expected);
+      });
+
+      it("Should return true if input is undefined", () => {
+        const data = undefined;
+        const expected = true;
+        const result = isNullOrEmptyOrUndefined(data);
+
+        expect(result).toStrictEqual(expected);
+      });
+
+      it("Should return true if input is empty", () => {
+        const data = "";
+        const expected = true;
+        const result = isNullOrEmptyOrUndefined(data);
+
+        expect(result).toStrictEqual(expected);
+      });
+
+      it("Should return true if input is only spaces", () => {
+        const data = "  ";
+        const expected = true;
+        const result = isNullOrEmptyOrUndefined(data);
+
+        expect(result).toStrictEqual(expected);
+      });
+
+      it("Should return false if input has value", () => {
+        const data = faker.random.word();
+        const expected = false;
+        const result = isNullOrEmptyOrUndefined(data);
 
         expect(result).toStrictEqual(expected);
       });
--- src/utils/is-null-or-undefined.ts
+++ src/utils/is-null-or-undefined.ts
@@ -2,5 +2,5 @@
 export const isNullOrUndefined = (value: any): value is null | undefined => value === null || value === undefined;
 
 export function isNullOrEmptyOrUndefined(value?: any | null): value is null | undefined {
-  return value === undefined || value === null || value === "" || value.toString().trim() === "";
+  return isNullOrUndefined(value) || value === "" || value.toString().trim() === "";
 }
