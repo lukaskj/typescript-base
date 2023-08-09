50b7d2b1e631096015bbd71c0446a3b91599b3bb
Lukas Jacomin <lukasjacomin@gmail.com>
2023-08-09 18:17:21 -0300

    fix build

--- esbuild/build.mjs
+++ esbuild/build.mjs
@@ -1,4 +1,39 @@
 import * as esbuild from "esbuild";
-import { esbuildConfig } from "./esbuild-config.mjs";
+import * as packageJson from "../package.json" assert { type: "json" };
 
-await esbuild.build(esbuildConfig);
+const sharedConfig = {
+  entryPoints: ["src/index.ts"],
+  // outfile: "dist/index.mjs",
+  bundle: true,
+  treeShaking: true,
+  minify: true,
+  // platform: "node",
+  // format: "esm",
+  // target: "es2016",
+  sourcemap: true,
+  keepNames: true,
+  logLevel: "info",
+  // banner: {
+  //   js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
+  // },
+  // plugins: [esbuildDecorators()],
+  external: Object.keys(packageJson.default.dependencies).concat(
+    Object.keys(packageJson.default.peerDependencies || {}),
+  ),
+};
+
+await esbuild.build({
+  ...sharedConfig,
+  platform: "node", // for CJS
+  outfile: "dist/index.js",
+});
+
+// await esbuild.build({
+//   ...sharedConfig,
+//   outfile: "dist/index.esm.js",
+//   platform: "neutral", // for ESM
+//   format: "esm",
+//   banner: {
+//     js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
+//   },
+// });
