b446ab357dbadb9b43576bdeee71b5f29bdb33ac
Lukas Jacomin <lukasjacomin@gmail.com>
2024-03-06 22:29:59 -0300

    chore: eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["@typescript-eslint/eslint-plugin"],
-  extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended", "eslint:recommended"],
   root: true,
   env: {
     node: true,
@@ -14,15 +14,25 @@ module.exports = {
   },
   ignorePatterns: [".eslintrc.js"],
   rules: {
+    "@typescript-eslint/explicit-function-return-type": "error",
     "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-unused-vars": ["error", { vars: "all", argsIgnorePattern: "^_" }],
+    "no-unused-vars": ["error", { vars: "all", argsIgnorePattern: "^_" }],
     "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "warn",
   },
   overrides: [
     {
-      files: ["**/*.controller.ts", "**/*.spec.ts", "**/*.fixture.ts"],
+      files: ["**/*controller.ts"],
       rules: {
+        "@typescript-eslint/explicit-function-return-type": "off",
+      },
+    },
+    {
+      files: ["**/*.spec.ts", "**/*.fixture.ts"],
+      rules: {
+        "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/explicit-function-return-type": "off",
       },
     },
